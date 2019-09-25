58f273a66e21a9a90fc4f757cc840031d5bd2264
毛瑞超 <44494806+ruichaomao@users.noreply.github.com>
2019-09-25 08:57:29 +0800

    文本操作

--- grasp_line.sh
+++ grasp_line.sh
@@ -1,1 +1,4 @@
+#取出log文件中带有“Value”的第二个匹配行
 for ff in `ls *log`;do sed -n '/Value/p' $ff | sed -n 2p >> s2s2.dat;done
+#取出log文件中带有“Value”的第二个匹配行的第九列
+for ff in `ls *log`;do sed -n '/Value/p' $ff | sed -n 1p | awk '{print $9}'>> s1s1.dat;done
