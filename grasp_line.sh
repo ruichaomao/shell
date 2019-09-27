789cba9cd3fe7e22167ad9a6d29bc4c3aafdc1c9
毛瑞超 <44494806+ruichaomao@users.noreply.github.com>
2019-09-27 20:26:06 +0800

    Update grasp_line.sh

--- grasp_line.sh
+++ grasp_line.sh
@@ -2,3 +2,5 @@
 for ff in `ls *log`;do sed -n '/Value/p' $ff | sed -n 2p >> s2s2.dat;done
 #取出log文件中带有“Value”的第二个匹配行的第九列
 for ff in `ls *log`;do sed -n '/Value/p' $ff | sed -n 1p | awk '{print $9}'>> s1s1.dat;done
+#将每一行的数字除以18然后输出到新文件中去
+for line in `cat 330_bcl850.dat`;do echo "scale=5;$line/18"| bc >> 330bcl850.dat;done
