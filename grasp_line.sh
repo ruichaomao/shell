90500b447ce65010b4ff55fe530197fce1df28d0
毛瑞超 <44494806+ruichaomao@users.noreply.github.com>
2019-10-11 08:54:52 +0800

    shell短指令

--- grasp_line.sh
+++ grasp_line.sh
@@ -4,3 +4,7 @@ for ff in `ls *log`;do sed -n '/Value/p' $ff | sed -n 2p >> s2s2.dat;done
 for ff in `ls *log`;do sed -n '/Value/p' $ff | sed -n 1p | awk '{print $9}'>> s1s1.dat;done
 #将每一行的数字除以18然后输出到新文件中去
 for line in `cat 330_bcl850.dat`;do echo "scale=5;$line/18"| bc >> 330bcl850.dat;done
+#将文件夹bcl_addh_gjf中的000.gjf 001.gjf...文件移动到frame0 1 2 ...9文件夹下
+for i in 0 1 2 3 4 5 6 7 8 9;do mkdir frame$i;done
+for i in 0 1 2 3 4 5 6 7 8 9;do mv bcl_addh_gjf/*"00$i"* frame$i;done
+
