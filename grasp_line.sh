09577d47f915dc21f5fdbc2be05c5b68985782c5
毛瑞超 <44494806+ruichaomao@users.noreply.github.com>
2019-10-12 20:04:12 +0800

    Update grasp_line.sh

--- grasp_line.sh
+++ grasp_line.sh
@@ -7,4 +7,5 @@ for line in `cat 330_bcl850.dat`;do echo "scale=5;$line/18"| bc >> 330bcl850.dat
 #将文件夹bcl_addh_gjf中的000.gjf 001.gjf...文件移动到frame0 1 2 ...9文件夹下
 for i in 0 1 2 3 4 5 6 7 8 9;do mkdir frame$i;done
 for i in 0 1 2 3 4 5 6 7 8 9;do mv bcl_addh_gjf/*"00$i"* frame$i;done
-
+#输出文件夹frame8中所有文件的相对路径，\为转义符
+find frame8/* | sed 's/^/.\//' > 888.txt
